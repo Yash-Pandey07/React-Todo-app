1d2b88d15a4a9866842a37b4b5e00b68740f0a8c
Yash-Pandey07 <ypandey777@yahoo.in>
2024-08-26 21:51:55 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+
+    const currentYear = new Date().getFullYear();
+    return <footer>
+    <p>Coptyright &copy; {currentYear} </p>
+    </footer>
+}
+export default Footer;
