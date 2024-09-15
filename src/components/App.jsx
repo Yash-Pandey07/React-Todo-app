7692a6e95f98dfb3b6f51e54cb87a43f81942b61
Yash-Pandey07 <ypandey777@yahoo.in>
2024-09-16 03:17:29 +0530

    Add & Delete button to Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "./notes";
+//import notes from "./notes";
 import CreateArea from "./CreateArea";
 
-
 function App() {
+  const [notes, setNotes] = useState([]);
+    
 
-    return <>
-        <Header />
-        <CreateArea />
-        <div className="notes-container">
-            {notes.map((noteItems) => (
-                <Note
-                    key={noteItems.key}
-                    title={noteItems.title}
-                    content={noteItems.content}
-                />
-            ))}
-        </div>
-        <Footer />
+    function addNote(newNote) {
+      //console.log(newNote);
+      setNotes((prevNotes) => {
+        return [...prevNotes, newNote];
+      });
+    }
+
+    function deleteNote(id){
+      // console.log("Delete")
+      setNotes(prevNotes => {
+        return prevNotes.filter((noteItems, index) => {
+          return index !== id;
+        });
+      });
+    }
+
+
+  return (
+    <>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      <div className="notes-container">
+        {notes.map((noteItems, index) => {
+          return <Note
+            key={index}
+            id={index}
+            title={noteItems.title}
+            content={noteItems.content}
+            onDelete={deleteNote}
+          />
+        })}
+      </div>
+      <Footer />
     </>
+  );
 }
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Footer() {
 
     const currentYear = new Date().getFullYear();
-    return <footer>
-    <p>Coptyright &copy; {currentYear} </p>
-    </footer>
+    return (
+        <footer>
+            <p>Copyright &copy; {currentYear}</p>
+        </footer>
+    );
 }
 export default Footer;
